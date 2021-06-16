19fcf41aa964c2240779308aa0b4d0d34a15c057
alexandre-dev-73 <avsantos2286211@gmail.com>
2021-06-16 09:57:03 -0300

    allow new entries and edit entries.

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -7,5 +7,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics', views.topics, name='topics'),
-    path(r'topics/(?p<topic_id>\d+)/$', views.topic, name='topic'),
+    path(r'topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    path(r'new_topic/$', views.new_topic, name='new_topic'),
+    path(r'new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+    path(r'edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Topic
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -21,3 +22,58 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Adiciona um novo assunto."""
+    if request.method != 'POST':
+        #Nenhum dado submetido; cria um formulário em branco
+        form = TopicForm()
+    else:
+        #Dados de POST submetidos; cria um formulario em branco
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Acrescenta uma nova entrada para um assunto em particular"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        #Nenhum dado submetido; cria um formulario em branco
+        form = EntryForm()
+    else:
+        #Dados de POST submetidos; processa os dados
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edita uma entrada existente."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        #Requisiçao inicial; preencher previamente o formulario com a entrada atual
+        form = EntryForm(instance=entry)
+    else:
+        #Dados de POST submetidos; processa os dados
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
